a751b08956457fca795ebfe70258988d46af454c
balajiaravind <85687886+balajiaravind@users.noreply.github.com>
2021-06-10 19:00:49 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System | Balaji KV</title>
+  </head>
+
+  <body>
+  <!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">Spark Foundation Bank</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="viewallcustomers.php">View Customers</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              
+          </div>
+       </nav>
+
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3>Welcome to</h3>
+                  <h1>Sparks Foundation</h1>
+				  <p>Project for #GRIPJUNE21</p>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+                <img src="img/bank.png" class="img-fluid pt-2">
+              </div>
+            </div>
+
+      <!-- Activity section -->
+            <div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="img/user.jpg" class="img-fluid">
+                    <br>
+                    <a href="viewallcustomers.php"><button>View Customers</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/transfer.jpg" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button>Transfer Amount</button></a>
+                  </div>
+                  
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2">
+        <p>&copy 2021. Made with &hearts; by <b>Balaji KV</b> <br>Intern @ MCC </p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
